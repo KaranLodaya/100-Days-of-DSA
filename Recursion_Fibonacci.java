03ad2702581e1a27bc17e0e5d44974b0d049b3df
KaranLodaya <karanlodaya363@gmail.com>
2023-12-09 17:47:07 +0530

    Finding the nth number in the Fibonacci series.

---
+++ Recursion_Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Recursion_Fibonacci { 
+    // recursion function
+    int Fibonacci(int n) {
+        
+        // base case
+        if ( n <= 1)
+        return n;
+
+        // small calculation with recursive call
+        // Recursive call being Fibonacci(n-1) & Fibonacci(n-2)
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the nth position");
+        int n = sc.nextInt();
+        Recursion_Fibonacci obj = new Recursion_Fibonacci();
+        System.out.println(obj.Fibonacci(n));
+        sc.close();
+    }
+}
